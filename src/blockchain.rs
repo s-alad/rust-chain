101d805726357eff3abb3c3191e466e9f6f76a87
s-alad <n7.saad@gmail.com>
2023-01-11 23:59:47 -0500

    err

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -17,37 +17,38 @@ pub struct Blockchain {
 }
 
 impl Blockchain {
-    pub fn verify(&mut self, block: Block) -> Result<(), > {
-        for (i, block) in self.blocks.iter().enumerate() {
-            if block.index != i as u32 {
-                println!("[{i}] - FAIL INDEX MISMATCH {} != {}", &block.index, &i);
-                return false;
-            }
-            else if (!block::difficulty(&block.hash(), block.difficulty)) {
-                println!("[{i}] - FAIL DIFFICULTY");
-                return false;
-            }
-            else if (i != 0) {
+    pub fn verify(&mut self, block: Block) -> Result<(), BlockValidationError> {
+        
+        let i = self.blocks.len();
 
-                let previousblock = &self.blocks[i - 1];
+        if block.index != i as u32 {
+            println!("[{i}] - FAIL INDEX MISMATCH {} != {}", &block.index, &i);
+            return false;
+        }
+        else if (!block::difficulty(&block.hash(), block.difficulty)) {
+            println!("[{i}] - FAIL DIFFICULTY");
+            return false;
+        }
+        else if (i != 0) {
 
-                if previousblock.time >= block.time  {
-                    println!("[{i}] - FAIL TIME");
-                    return false
-                }
+            let previousblock = &self.blocks[i - 1];
 
-                if previousblock.hash != block.previous {
-                    println!("[{i}] - FAIL HASH");
-                    return false
-                }
+            if previousblock.time >= block.time  {
+                println!("[{i}] - FAIL TIME");
+                return false
             }
-            else {
-                if block.previous != vec![0; 32] {
-                    println!("[{i}] - FAIL GENISIS");
-                    return false
-                }
+
+            if previousblock.hash != block.previous {
+                println!("[{i}] - FAIL HASH");
+                return false
             }
         }
-        return true;
+        else {
+            if block.previous != vec![0; 32] {
+                println!("[{i}] - FAIL GENISIS");
+                return false
+            }
+        }
+        
     }
 }
